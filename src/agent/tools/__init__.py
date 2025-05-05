10f51b0e1393895f0d5009e3ade7331a26d0828f
nsviel <nathan.sanchizviel@pm.me>
2025-05-05 16:00:41 +0200

    add_new_tools

--- src/agent/tools/__init__.py
+++ src/agent/tools/__init__.py
@@ -1,5 +1,5 @@
 from tools.improver import improver
 from tools.scene import SceneAnalyzer
-from tools.decomposer import Decomposer
+from tools.decomposer import decomposer
 from tools.date import date
 from tools.vision import image_analysis
