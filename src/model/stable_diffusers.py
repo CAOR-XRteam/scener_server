ad9a3eff6f9f9d357ea808c849e5fc982a9b18f2
artem <aburnistov05@gmail.com>
2025-07-03 12:47:54 +0200

    stable diffusion

--- src/model/stable_diffusers.py
+++ src/model/stable_diffusers.py
@@ -1,17 +1,15 @@
 import torch
 
 from beartype import beartype
-from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
+from diffusers import StableDiffusion3Pipeline, EulerDiscreteScheduler
 
 
 @beartype
 def generate(prompt: str, filename: str):
-    model_id = "stabilityai/stable-diffusion-2-1"
+    model_id = "stabilityai/stable-diffusion-3.5-medium"
 
-    scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
-    pipe = StableDiffusionPipeline.from_pretrained(
-        model_id, scheduler=scheduler, torch_dtype=torch.float16
-    )
+    # scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
+    pipe = StableDiffusion3Pipeline.from_pretrained(model_id, torch_dtype=torch.float16)
     pipe = pipe.to("cuda")
 
     image = pipe(prompt).images[0]
