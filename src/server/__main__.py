d93231632389dc6111690164f774c2a129fe39d3
arteume <aburnistov05@gmail.com>
2025-10-08 13:41:52 +0200

    env

--- src/server/__main__.py
+++ src/server/__main__.py
@@ -4,7 +4,7 @@ import os
 
 if __name__ == "__main__":
     os.system("clear")
-    HOST = os.getenv("HOST", "localhost")
-    PORT = int(os.getenv("PORT", 8765))
+    HOST = os.getenv("WEBSOCKET_HOST", "localhost")
+    PORT = int(os.getenv("WEBSOCKET_PORT", 8765))
     server = ServerAPI(host=HOST, port=PORT)
     server.start()
